eabaa4a45e3a8b704a84508c82ab4c0fb9af46a6
YU-HSI JEN <yuhsi.jen@gmail.com>
2025-07-06 16:36:03 +0900

    add book class

---
+++ ProGitForProgrammers/Book.cs
@@ -0,0 +1,12 @@
+namespace ProGitForProgrammers
+{
+    class Book
+    {
+        public string Title { get; set; }
+
+        public List<string> Authors { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+
+    }
+}
