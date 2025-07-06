8fd35c48d9a2b4ad5db7cfc6003ddb8036973603
YU-HSI JEN <yuhsi.jen@gmail.com>
2025-07-06 13:15:37 +0900

    testing where the HEAD tag would show from VS

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -6,6 +6,8 @@
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("I just added this in Visual Studio");
+            Console.WriteLine("I just added this in the command line repo");
+            Console.WriteLine("This line add to show the staging area");
         }
     }
 }
